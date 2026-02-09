c6824b21bf5bbdc410f3e303fe07961c25cbfd2c
Nguyen Hoang Thang <nhoangthang972@gmail.com>
2026-02-09 18:09:54 +0700

    Move app.py to root directory for Render

--- app.py
+++ app.py
@@ -130,4 +130,5 @@ def predict_churn(customer: CustomerProfile):
 
 # Run the server if the file is to be executed directly.
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
